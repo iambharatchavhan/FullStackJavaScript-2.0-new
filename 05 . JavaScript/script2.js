de047081d24ee8d7212a84f66dbc6cc5687a9214
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-01-05 20:01:21 +0530

    javascript array practice 01

---
+++ 05 . JavaScript/script2.js
@@ -0,0 +1,14 @@
+// copying an array using function
+
+// const numbers=[1,2,3,4,5,6,7,8,9]
+
+const squares=(numbers)=> {
+    let result =[]; 
+
+    for (number of numbers){
+        result.push(number * 2);
+    }
+    return result;
+}
+
+console.log(result);
