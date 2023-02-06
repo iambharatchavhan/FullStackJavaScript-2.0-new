76c04fe452f3477d4b4e11d2884c49cdbba5e97c
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-02-06 20:46:30 +0530

    dom 4th solve

--- 08. DOM_Assignments/04_DOM Project/04_DOM Project/index.js
+++ 08. DOM_Assignments/04_DOM Project/04_DOM Project/index.js
@@ -1,4 +1,44 @@
+let statNum = document.getElementsByClassName('stat')
+let statValue = document.getElementsByClassName('stat-value')
 
-// const barbarian = document.getElementsByClassName("clash-card__unit-stats--barbarian");
 
-// const test = document.getElementsByClassName('stat').style.color= "red";
+
+
+function allClasses (className){
+    for(i=0 ; i < className.length; i++){
+        className[i].style.color = "#ffffff";
+    }
+}
+allClasses(statNum);
+allClasses(statValue);
+
+//& background color 
+
+const giant = document.getElementsByClassName("clash-card__unit-stats--barbarian")[0];
+
+giant.style.backgroundColor = "#A57512"
+
+//^---------
+
+const archer = document.getElementsByClassName("clash-card__unit-stats--archer")[0];
+
+archer.style.backgroundColor = "#C43A7C"
+
+
+//^---------
+const theGient = document.getElementsByClassName("clash-card__unit-stats--giant")[0];
+
+theGient.style.backgroundColor = "#F6901A"
+
+//^---------
+
+const goblin = document.getElementsByClassName("clash-card__unit-stats--goblin")[0];
+
+goblin.style.backgroundColor = "#99CC3A"
+
+//^---------
+const wizard = document.getElementsByClassName("clash-card__unit-stats--wizard")[0];
+
+wizard.style.backgroundColor = "#63A2DC"
+
+//^---------
