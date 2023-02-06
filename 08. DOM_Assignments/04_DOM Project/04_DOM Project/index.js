7c49382c15569fa5da9f8fc4401b0e7d1654fadb
Bharat Chavhan <bharatchavhan141@gmail.com>
2023-02-06 21:33:33 +0530

    dom 4th solve

--- 08. DOM_Assignments/04_DOM Project/04_DOM Project/index.js
+++ 08. DOM_Assignments/04_DOM Project/04_DOM Project/index.js
@@ -3,42 +3,22 @@ let statValue = document.getElementsByClassName('stat-value')
 
 
 
-
 function allClasses (className){
-    for(i=0 ; i < className.length; i++){
+    for( i=0 ; i < className.length; i++){
         className[i].style.color = "#ffffff";
     }
 }
 allClasses(statNum);
 allClasses(statValue);
 
+
+let cards = Array.from((document.getElementsByClassName('clearfix')))
+
+const colors = ["#A57512","#C43A7C","#F6901A","#99CC3A","#63A2DC"]
+
+for(let i=0; i < cards.length; i++ ){
+    cards[i].style.backgroundColor = colors[i];
+}
+
+
 //& background color 
-
-const giant = document.getElementsByClassName("clash-card__unit-stats--barbarian")[0];
-
-giant.style.backgroundColor = "#A57512"
-
-//^---------
-
-const archer = document.getElementsByClassName("clash-card__unit-stats--archer")[0];
-
-archer.style.backgroundColor = "#C43A7C"
-
-
-//^---------
-const theGient = document.getElementsByClassName("clash-card__unit-stats--giant")[0];
-
-theGient.style.backgroundColor = "#F6901A"
-
-//^---------
-
-const goblin = document.getElementsByClassName("clash-card__unit-stats--goblin")[0];
-
-goblin.style.backgroundColor = "#99CC3A"
-
-//^---------
-const wizard = document.getElementsByClassName("clash-card__unit-stats--wizard")[0];
-
-wizard.style.backgroundColor = "#63A2DC"
-
-//^---------
